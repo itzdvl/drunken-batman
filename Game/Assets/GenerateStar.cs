d35489523617333b931890299d3145120b304650
itzdvl <itzdvl@gmail.com>
2013-09-14 13:17:14 -0500

    Added Star Generator

---
+++ Game/Assets/GenerateStar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateStar : MonoBehaviour {
+
+	public GameObject star;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (star);
+		star.transform.position = new Vector3 (0, 0, 0);
+		//sphere.transform.scale = Vector3(1,1,1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
